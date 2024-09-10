8b3bf6827693afa882ddcb0f68a842c949594135
Rodrigo Cabral <rodcabral.ct@gmail.com>
2024-09-10 15:04:09 -0300

    check the current path

--- src/ultra.c
+++ src/ultra.c
@@ -1,26 +1,3 @@
-/*
-Copyright (c) 2024 Rodrigo Cabral (rodcabral)
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-
-
 #include "ultra.h"
 #include <stdlib.h>
 #include <string.h>
@@ -51,9 +28,38 @@ Queue *init_queue() {
     return queue;
 }
 
-int ultra_get(UltraServer *server, const char *path, const char *file_path) {
+int _ultra_current_path(const char* path) {
+    // TODO: Get the real request message
+    const char* request = "GET /file.html HTTP/1.1";
+
+    int start  = 0;
+    for(int i = 0; i < strlen(request); ++i) {
+        if(request[i] == '/') {
+            start = i;
+            break;
+        }
+    }
+
+    char *current_path = malloc(255);
+
+    int j = 0;
+    for(int i = start; request[i] != ' '; ++i) {
+        current_path[j++] = request[i];
+    }
+    current_path[j] = '\0';
+
+    if(strncmp(path, current_path, 255) == 0) {
+        free(current_path);
+        return 1;
+    }
+    free(current_path);
 
     return 0;
+}
+
+int ultra_current_path(const char *path) {
+    int val = _ultra_current_path(path);
+    return val;
 }
 
 int _ultra_get(int* clientfd) {
