363e9f064e458561b509e3ae2094165347eed3df
Rodrigo Cabral <rodcabral.ct@gmail.com>
2024-09-11 17:09:40 -0300

    examples

--- examples/hello.c
+++ examples/hello.c
@@ -1,8 +1,15 @@
 #include "../src/ultra.h"
 
+void handle(int* fd) {
+    if(ultra_current_path(fd, "/")) {
+        ultra_get(fd, "./examples/index.html");
+    } else {
+        ultra_get(fd, "./examples/404.html");
+    }
+}
+
 int main(void) {
     UltraServer server = ultra_init(8080);
-
-    ultra_connect(&server);
+    ultra_connect(&server, handle);
     return 0;
 }
