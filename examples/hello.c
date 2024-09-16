83744150820394e219401737469db36a7f69e5cc
Rodrigo Cabral <rodcabral.ct@gmail.com>
2024-09-16 00:21:24 -0300

    new example

--- examples/hello.c
+++ examples/hello.c
@@ -1,23 +1,22 @@
 #include "../src/ultra.h"
-#include <stdio.h>
 #include <string.h>
 
 void handle(int *fd) {
-    printf("Connected: %d\n", *fd);
     UltraRequest request = ultra_request(fd);
+    UltraResponse response = ultra_response(fd);
 
     if(strncmp(request.path, "/", 255) == 0) {
-        ultra_send_file(fd, "./examples/index.html");
+        ultra_send_file(&response, "./examples/index.html");
     } else if(strncmp(request.path, "/something", 255) == 0) {
-        ultra_send_file(fd, "./examples/something.html");
+        ultra_send_file(&response, "./examples/something.html");
     } else {
-        ultra_send_file(fd, "./examples/404.html");
+        response.status_code = 404;
+        ultra_send_file(&response, "./examples/404.html");
     }
 }
 
 int main(void) {
     UltraServer server = ultra_init(8080);
     ultra_connect(&server, handle);
-
     return 0;
 }
