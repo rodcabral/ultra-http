44d48126062c92f0c4b88057504e43699349acb5
Rodrigo Cabral <rodcabral.ct@gmail.com>
2024-11-11 10:48:56 -0300

    create and delete json struct

--- src/ultra-json/ultra-json.c
+++ src/ultra-json/ultra-json.c
@@ -1,1 +1,26 @@
 #include "./ultra-json.h"
+
+UltraJson *ultra_json(char* json) {
+    UltraJson* ujson = (UltraJson*)malloc(sizeof(UltraJson));
+
+    if(!json) {
+        return NULL;
+    }
+
+    ujson->data = json;
+    ujson->parsed = malloc(strlen(json));
+
+    return ujson;
+}
+
+void ultra_json_close(UltraJson* ujson) {
+    if(!ujson) return;
+
+    ujson->data = NULL;
+
+    if(ujson->parsed) {
+        free(ujson->parsed);
+    }
+
+    free(ujson);
+}
--- src/ultra-json/ultra-json.h
+++ src/ultra-json/ultra-json.h
@@ -2,5 +2,17 @@
 #define _ULTRA_JSON_H_
 
 #include <inttypes.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+typedef struct {
+    char* parsed;
+    char* data;
+} UltraJson;
+
+UltraJson *ultra_json(char* json);
+
+void ultra_json_close(UltraJson* ujson);
 
 #endif
