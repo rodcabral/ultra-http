00825ce643bb9c196bba2a41b2bc48cdb69c1e41
Rodrigo Cabral <rodcabral.ct@gmail.com>
2024-11-11 20:01:59 -0300

    create custom stack

--- src/ultra-json/ultra-json.c
+++ src/ultra-json/ultra-json.c
@@ -1,5 +1,78 @@
 #include "./ultra-json.h"
 
+typedef struct _Node {
+    char ch;
+    struct _Node *prev;
+    struct _Node *next;
+} Node;
+
+typedef struct {
+    Node* head;
+    Node* tail;
+} Stack;
+
+Stack* init_stack() {
+    Stack* stack = (Stack*)malloc(sizeof(Stack));
+    
+    stack->head = NULL;
+    stack->tail = NULL;
+
+    return stack;
+}
+
+void delete_stack(Stack* stack) {
+    if(!stack->head) return;
+
+    Node* temp = stack->head;
+
+    while(stack->head) {
+        temp = stack->head;
+        stack->head  = stack->head->next;
+        free(temp);
+    }
+    
+    free(stack);
+}
+
+void push(Stack* stack, char ch) {
+    Node* temp = (Node*)malloc(sizeof(Node));
+    temp->ch = ch;
+
+    if(!stack->head) {
+        stack->head = temp;
+        stack->tail = temp;
+        return;
+    }
+
+    temp->next = NULL;
+    temp->prev = stack->tail;
+
+    stack->tail->next = temp;
+    stack->tail = temp;
+}
+
+char pop(Stack* stack) {
+    Node* temp = stack->tail;
+    char ch = temp->ch;
+
+    if(temp->prev) {
+        stack->tail = temp->prev;
+    }
+
+    if(temp == stack->head) {
+        free(stack->head);
+        stack->head = NULL;
+        return ch;
+    }
+
+    if(temp) {
+        free(temp);
+        temp = NULL;
+    }
+
+    return ch;
+}
+
 UltraJson *ultra_json(char* json) {
     UltraJson* ujson = (UltraJson*)malloc(sizeof(UltraJson));
 
@@ -9,6 +82,10 @@ UltraJson *ultra_json(char* json) {
 
     ujson->data = json;
     ujson->parsed = malloc(strlen(json));
+
+    Stack* stack = init_stack();
+
+    delete_stack(stack);
 
     return ujson;
 }
