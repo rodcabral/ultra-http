9443fb00cd4b61899b739e7fe31a294aa4d01744
Rodrigo Cabral <rodcabral.ct@gmail.com>
2024-11-27 20:15:44 -0300

    examples

--- examples/hello.c
+++ examples/hello.c
@@ -1,19 +1,19 @@
 #include "../src/ultra.h"
 
 void handle(int *fd) {
-    UltraRequest* request = ultra_request(fd);
-    UltraResponse* response = ultra_response(fd);
+    UltraRequest request = ultra_request(fd);
+    UltraResponse response = ultra_response(fd);
 
-    if(ultra_get(request, "/")) {
-        ultra_send(response, "hello, world!");
+    if(ultra_get(&request, "/")) {
+        ultra_send(&response, "hello, world!");
     }
 
-    if(ultra_post(request, "/")) {
-        response->status = 201;
-        ultra_send(response, request->body);
+    if(ultra_post(&request, "/")) {
+        response.status = 201;
+        ultra_send(&response, request.body);
     }
 
-    ultra_close(request, response);
+    ultra_close(&request);
 }
 
 int main() {
