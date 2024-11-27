96f9f7f350d765c000002f140ccd145724c0af78
Rodrigo Cabral <rodcabral.ct@gmail.com>
2024-11-27 11:54:19 -0300

    example

--- examples/hello.c
+++ examples/hello.c
@@ -1,16 +1,15 @@
 #include "../src/ultra.h"
 
-void handle(int* fd) {
-    ultra_json_init();
-
+void handle(int *fd) {
     UltraRequest* request = ultra_request(fd);
-    UltraResponse* response = ultra_response(fd, request);
+    UltraResponse* response = ultra_response(fd);
 
     if(ultra_get(request, "/")) {
         ultra_send(response, "hello, world!");
     }
 
     if(ultra_post(request, "/")) {
+        response->status = 201;
         ultra_send(response, request->body);
     }
 
